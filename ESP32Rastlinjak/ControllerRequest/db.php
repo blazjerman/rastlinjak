4cfce3c6d3f8ca808012321bb8e6d9b83a2bec4e
blazjerman <77243499+blazjerman@users.noreply.github.com>
2023-03-22 14:18:08 +0100

    Delete ESP32Rastlinjak/ControllerRequest directory

--- ESP32Rastlinjak/ControllerRequest/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'codeigniter');
-   define('DB_PASSWORD', 'codeigniter2019');
-   define('DB_DATABASE', 'Rastlinjak');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
