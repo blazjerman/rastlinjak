b88ad5669f01d1d54633d2b36ada26ee230ddfc3
AndrejErjavec <erandrej@gmail.com>
2023-03-27 22:36:24 +0200

    GET last sensor measurement

--- backend/routes/esp.js
+++ backend/routes/esp.js
@@ -2,4 +2,31 @@ const express = require('express');
 const router = express.Router();
 const esp32 = require('../models/ESP32');
 
+router.get('/info', async (req,res) => {
+    const id = req.query.id;
+
+    if (!id) {
+        res.status(404).json({
+            message: 'ID not provided'
+        });
+    }
+
+    const info = await esp32.findOne({
+        attributes: ['id', 'mac', 'update_interval'],
+        where: {
+            id: id
+        }
+    });
+
+    if (!info) {
+        res.status(404).json({
+            message: 'ESP not found'
+        });
+    }
+
+    res.status(200).json({
+        info
+    });
+});
+
 module.exports = router;
--- backend/routes/sensors.js
+++ backend/routes/sensors.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Temperature = require('../models/Temperature');
 
+// =============== temperature ===============
+
 router.get('/temperature', async (req, res) => {
   let sensor_id = req.query.sensor_id;
   let ESP32_id = req.query.esp_id;
@@ -22,11 +24,34 @@ router.get('/temperature', async (req, res) => {
   });
 });
 
+router.get('/temperature/now', async(req, res) => {
+  let sensor_id = req.query.sensor_id;
+  let ESP32_id = req.query.esp_id;
+
+  if (!sensor_id) { sensor_id = 0; }
+  if (!ESP32_id) { ESP32_id = 0; }
+
+  const lastTemp = await Temperature.findOne({
+    attributes: ['value', 'time'],
+    where: {
+      ESP32_id: ESP32_id,
+      sensor_id: sensor_id
+    },
+    order: [[ 'time', 'DESC' ]],
+  });
+
+  res.status(200).json({
+    lastTemp
+  })
+});
+
+// =============== humidity ===============
+
 router.get('/humidity', async (req, res) => {
   let sensor_id = req.query.sensor_id;
   let ESP32_id = req.query.esp_id;
 
-  if (!sensor_id){ sensor_id = 0; }
+  if (!sensor_id) { sensor_id = 0; }
   if (!ESP32_id) { ESP32_id = 0; }
     
   const humData = await Temperature.findAll({
@@ -41,6 +66,29 @@ router.get('/humidity', async (req, res) => {
     humData
   });
 });
+
+router.get('/humidity/now', async (req, res) => {
+  let sensor_id = req.query.sensor_id;
+  let ESP32_id = req.query.esp_id;
+
+  if (!sensor_id) { sensor_id = 0; }
+  if (!ESP32_id) { ESP32_id = 0; }
+    
+  const lastHumidity = await Humidity.findOne({
+    attributes: ['value', 'time'],
+    where: {
+      ESP32_id: ESP32_id,
+      sensor_id: sensor_id
+    },
+    order: [[ 'time', 'DESC' ]]
+  });
+
+  res.status(200).json({
+    lastHumidity
+  });
+});
+
+// =============== pressure ===============
 
 router.get('/pressure', async (req, res) => {
   let sensor_id = req.query.sensor_id;
@@ -59,6 +107,27 @@ router.get('/pressure', async (req, res) => {
 
   res.status(200).json({
     pressureData
+  });
+});
+
+router.get('/pressure/now', async (req, res) => {
+  let sensor_id = req.query.sensor_id;
+  let ESP32_id = req.query.esp_id;
+
+  if (!sensor_id){ sensor_id = 0; }
+  if (!ESP32_id) { ESP32_id = 0; }
+    
+  const lastPressure = await Temperature.findOne({
+    attributes: ['value', 'time'],
+    where: {
+      ESP32_id: ESP32_id,
+      sensor_id: sensor_id
+    },
+    order: [[ 'time', 'DESC' ]]
+  });
+
+  res.status(200).json({
+    lastPressure
   });
 });
 
